066c88708391fd1362c13a9d07d58baaf185ffe2
Daniel Harris <bobth3bum@gmail.com>
2015-03-11 14:32:21 -0600

    add friends and insults

--- app/models/tweet_fetcher.rb
+++ app/models/tweet_fetcher.rb
@@ -1,5 +1,11 @@
 class TweetFetcher
 
+  require 'open-uri'
+
+  attr_reader :client
+  attr_reader :insult
+
+
   def initialize
     @user = "bobth3bum"
     @client = Twitter::REST::Client.new do |config|
@@ -8,9 +14,18 @@ class TweetFetcher
       config.access_token        = ENV["ACCESS_TOKEN"]
       config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
     end
+    @friends = [@rey_deloge, @Jess_Koch, @ajamesDesign13, @hkblackstock, @demullane,
+      @JordanICollier, @alexandrahanley, @cws322, @ooesili, @KennethT, @caseyK819,
+      @ahumanmackenzie, @missylaneuz, @_calebatwood, @tiltedtower, @JakeMarwil, @melissakhat,
+      @kerryhouchin, @TyHyten, @fbhewitt]
+    @file = open('http://corz.org/public/docs/miscy/insults.txt?raw=true')
+    @contents = @file.read
+    @insults = @contents.split("\r\n")
+    @insult = @insults[rand(@insults.length)]
   end
 
   def tweet
-    @client.update("second twitter and github api test")
+    random = @friends[rand(@friends.length)]
+    @client.update("#{random} - #{insult}")
   end
 end
